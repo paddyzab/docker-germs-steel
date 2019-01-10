fa7c4ef12189ba1b5c7b5a57435f726d6296d5d7
Patryk Zabicki <patryk.zabicki@gmail.com>
2019-01-10 20:10:10 +0100

    Changing the text for branch testing

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React NOW, or later!
+            Hello from the Feature branch!
           </a>
         </header>
       </div>
